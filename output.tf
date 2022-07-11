7398bafcdabe4715aa11328825a5f916a3784e84
Saikishore031 <50044342+Saikishore031@users.noreply.github.com>
2022-07-11 20:46:28 +0530

    Add files via upload

--- output.tf
+++ output.tf
@@ -1,14 +1,6 @@
-output "nic-id" {
-  value       = azurerm_network_interface.nics.*.id
-  description = "The ID of the Network Interface."
+output "pe_obj" {
+  value = azurerm_private_endpoint.pe
 }
 
-output "vm-id" {
-  value       = azurerm_windows_virtual_machine.node.*.id
-  description = "The ID of the Windows Virtual Machine."
-}
-
-output "private_ip" {
-  value       = azurerm_windows_virtual_machine.node.private_ip_address
-  description = "The Primary Private IP Address assigned to this Virtual Machine."
-}
+output "nic_obj" {
+value = azurerm_private_endpoint.pe.network_interface.id
